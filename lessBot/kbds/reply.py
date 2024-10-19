a869afed2ddb56cb453bc77d5008c2548fc79d9e
vaceslaveremin <irbis19822801@gmail.com>
2024-10-19 19:00:09 +0400

    Добавил reply клавиатуру
    Signed-off-by: vaceslaveremin <irbis19822801@gmail.com>

---
+++ lessBot/kbds/reply.py
@@ -0,0 +1,12 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+start_cb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+        KeyboardButton(text="Меню"),
+        KeyboardButton(text="О магазине"),
+        KeyboardButton(text="Варианты доставки"),
+        KeyboardButton(text="Варианты оплаты"),
+        ]
+    ]
+)
