c0fb37969fb4e1d5c4848c6fc35d6b3bd92b5cee
vaceslaveremin <irbis19822801@gmail.com>
2024-10-20 10:35:35 +0400

    разбил клавиатуру start_cb на две строки Добавил поясняющий коментарий
    Signed-off-by: vaceslaveremin <irbis19822801@gmail.com>

--- lessBot/kbds/reply.py
+++ lessBot/kbds/reply.py
@@ -2,10 +2,12 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 start_cb = ReplyKeyboardMarkup(
     keyboard=[
+        # Первый ряд кнопок
         [
         KeyboardButton(text="Меню"),
         KeyboardButton(text="О магазине"),
         ],
+        # Второй ряд кнопок
         [
         KeyboardButton(text="Варианты доставки"),
         KeyboardButton(text="Варианты оплаты"),
