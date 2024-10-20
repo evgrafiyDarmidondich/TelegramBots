6c87dda21649d380d5755ec445caff4039c282bb
vaceslaveremin <irbis19822801@gmail.com>
2024-10-20 10:34:19 +0400

    разбил клавиатуру start_cb на две строки
    Signed-off-by: vaceslaveremin <irbis19822801@gmail.com>

--- lessBot/kbds/reply.py
+++ lessBot/kbds/reply.py
@@ -5,9 +5,11 @@ start_cb = ReplyKeyboardMarkup(
         [
         KeyboardButton(text="Меню"),
         KeyboardButton(text="О магазине"),
+        ],
+        [
         KeyboardButton(text="Варианты доставки"),
         KeyboardButton(text="Варианты оплаты"),
-        ]
+        ],
     ],
     resize_keyboard=True,
     input_field_placeholder='Что вас интересует?'
