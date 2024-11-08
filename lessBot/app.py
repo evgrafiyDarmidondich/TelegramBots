5c752d155ea549d7815f8b0f59638aa3e097cb32
mac <irbis19822801@gmail.com>
2024-11-08 12:37:47 +0400

    добавил создание бд

--- lessBot/app.py
+++ lessBot/app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 
 from lessBot.common.bot_cmds_list import privat
-from lessBot.database.engine import create_db
+from lessBot.database.engine import create_db, drop_db
 from lessBot.hendlers.admin_privat import admin_router
 from lessBot.hendlers.user_group import user_group_router
 
@@ -22,7 +22,7 @@ from lessBot.hendlers.user_privat import user_private_router
 TOKEN = os.getenv('TOKEN')
 
 # список разрешоных обновлений
-ALLOWED_UPDATES = ['message', 'edited_message']
+ALLOWED_UPDATES = ['message', 'edited_message', 'callback_query']
 
 bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 bot.my_admins_list = []
@@ -35,11 +35,23 @@ dp.include_router(admin_router)
 dp.include_router(user_private_router)
 dp.include_router(user_group_router)
 
+async def on_startup(bot):
 
+    run_param = False
+    if run_param:
+        await drop_db()
+
+    await create_db()
+
+async def on_shutdown(bot):
+    print('Бот лёг')
 
 
 # функция запуска бота
 async def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
     # Запуск создания базы данных
     await create_db()
     # Сбрасывает ожидаеме обновления
@@ -47,7 +59,7 @@ async def main():
     # вызов програмных кнопок меню
     await bot.set_my_commands(commands=privat, scope=BotCommandScopeAllPrivateChats())
 
-    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 # Вызвали функцию запуска бота
 asyncio.run(main())
--- lessBot/database/engine.py
+++ lessBot/database/engine.py
@@ -1,10 +1,12 @@
 import os
+from dotenv import load_dotenv
 
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from lessBot.database.models import Base
+load_dotenv()
 
-engine = create_async_engine(os.getenv(DB_LITE), echo=True)
+engine = create_async_engine(os.getenv('DB_LITE'), echo=True)
 
 session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
